5e9046047ca631df71e7d966d24a82d3ee19a1ae
Blas Rabella <blas.rabella@hotmail.com>
2018-07-16 09:51:20 +0200

    Formatting according to guidelines

--- rust-solution/src/main.rs
+++ rust-solution/src/main.rs
@@ -1,10 +1,10 @@
 extern crate image;
 extern crate rayon;
 
-use image::{GrayImage};
+use image::GrayImage;
 use std::env;
 mod solver;
-use solver::{solve, check_error};
+use solver::{check_error, solve};
 
 fn help() {
     println!("Waldo test solver.\nThe program needs the path to the two images to test. Ex: waldo-test img1 img2")
@@ -17,23 +17,24 @@ fn main() {
             let img1_path = &args[1];
             let img2_path = &args[2];
             let (img1, img2) = read_images(&img1_path, &img2_path);
-            let (i,j,err) = solve(&img1, &img2).expect("Template not found in image");
-            if check_error(err, &img2){
-                println!("Found template at pixel [{:?}]",(i,j))
-            }else{
+            let (i, j, err) = solve(&img1, &img2).expect("Template not found in image");
+            if check_error(err, &img2) {
+                println!("Found template at pixel [{:?}]", (i, j))
+            } else {
                 println!("Template not found in image")
             }
-           
         }
-        _ => {
-            help()
-        }
+        _ => help(),
     }
 }
 
-fn read_images(path1: &str, path2: &str) -> (GrayImage, GrayImage){
-    let img1 = image::open(path1).expect("Couldn't read the first image").to_luma();
-    let img2 = image::open(path2).expect("Couldn't read the second image").to_luma();
+fn read_images(path1: &str, path2: &str) -> (GrayImage, GrayImage) {
+    let img1 = image::open(path1)
+        .expect("Couldn't read the first image")
+        .to_luma();
+    let img2 = image::open(path2)
+        .expect("Couldn't read the second image")
+        .to_luma();
     let (w1, h1) = img1.dimensions();
     let (w2, h2) = img2.dimensions();
     if w1 < w2 || h1 < h2 {
--- rust-solution/src/solver.rs
+++ rust-solution/src/solver.rs
@@ -1,23 +1,23 @@
-use image::{GrayImage};
+use image::GrayImage;
 use rayon::prelude::*;
 
-pub fn check_error(err: i32, img: &GrayImage) -> bool{
-    let (w,h) = img.dimensions();
-    let threshold: i32 = (w*h*5) as i32;
+pub fn check_error(err: i32, img: &GrayImage) -> bool {
+    let (w, h) = img.dimensions();
+    let threshold: i32 = (w * h * 5) as i32;
     if threshold > err {
         true
-    }else{
+    } else {
         false
     }
 }
 
-pub fn solve(img1: &GrayImage, img2: &GrayImage) -> Option<(u32, u32, i32)>{
+pub fn solve(img1: &GrayImage, img2: &GrayImage) -> Option<(u32, u32, i32)> {
     let (_w1, h1) = img1.dimensions();
     let (_w2, h2) = img2.dimensions();
-    let result = (0..h1-h2).into_par_iter()
-        .filter_map( |j| {
-            check_line(j, &img1, &img2)  
-        }).min_by(|(_,_,sq1), (_,_,sq2)| sq1.cmp(sq2));
+    let result = (0..h1 - h2)
+        .into_par_iter()
+        .filter_map(|j| check_line(j, &img1, &img2))
+        .min_by(|(_, _, sq1), (_, _, sq2)| sq1.cmp(sq2));
 
     result
 }
@@ -25,24 +25,26 @@ pub fn solve(img1: &GrayImage, img2: &GrayImage) -> Option<(u32, u32, i32)>{
 fn check_line(j: u32, img1: &GrayImage, img2: &GrayImage) -> Option<(u32, u32, i32)> {
     let (w1, _h1) = img1.dimensions();
     let (w2, _h2) = img2.dimensions();
-    let max_w = w1-w2;
-    (0..max_w).into_par_iter().map( |i|{
-        let sqt_error = check_img(i,j,&img1,&img2);
-        (i,j,sqt_error)
-    }).min_by(|(_,_,sq1), (_,_,sq2)| sq1.cmp(sq2))
-    
+    let max_w = w1 - w2;
+    (0..max_w)
+        .into_par_iter()
+        .map(|i| {
+            let sqt_error = check_img(i, j, &img1, &img2);
+            (i, j, sqt_error)
+        })
+        .min_by(|(_, _, sq1), (_, _, sq2)| sq1.cmp(sq2))
 }
 
-fn check_img(i: u32, j: u32, img1: &GrayImage, img2: &GrayImage) -> i32{
+fn check_img(i: u32, j: u32, img1: &GrayImage, img2: &GrayImage) -> i32 {
     let (w, h) = img2.dimensions();
-    let mut sum_err :i32 = 0;
-    for i2 in 0..(w){
-        for j2 in 0..(h){
-            let a = img2.get_pixel(i2,j2)[0] as i32;
-            let b = img1.get_pixel(i+i2, j+j2)[0] as i32;
-            let diff = (a-b).pow(2);
+    let mut sum_err: i32 = 0;
+    for i2 in 0..(w) {
+        for j2 in 0..(h) {
+            let a = img2.get_pixel(i2, j2)[0] as i32;
+            let b = img1.get_pixel(i + i2, j + j2)[0] as i32;
+            let diff = (a - b).pow(2);
             sum_err = sum_err + diff;
-         }
+        }
     }
     sum_err
 }
